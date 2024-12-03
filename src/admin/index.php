c2e89fa44836af60be053602e27dee38d52ed793
agomezEVG <agomezdelgado.guadalupe@alumnado.fundacionloyola.net>
2024-12-03 16:32:05 +0100

    Reestructuración de directorios 3

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,8 +1,6 @@
 <?php
     require_once 'app/config/config.php';
 
-    
-
     if(!isset($_GET['controlador'])) $_GET['controlador'] = CONTROLADOR_POR_DEFECTO;
     if(!isset($_GET['metodo'])) $_GET['metodo'] = METODO_POR_DEFECTO;
 
@@ -17,7 +15,7 @@
     $datos = [];
 
     if(method_exists($objControlador, $_GET['metodo'])) {
-        $datos = $objControlador->{$_GET['metodo']}(); 
+        $datos = $objControlador->{$_GET['metodo']}();
         echo json_encode($datos);
     }
 ?>
