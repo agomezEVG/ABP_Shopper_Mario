6656e28b09cfb777c87cd5a2bdffe24a4568d5dc
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2024-12-16 08:33:50 +0100

    cambio de servidores

--- src/admin/app/models/m_listarTipos.php
+++ src/admin/app/models/m_listarTipos.php
@@ -8,7 +8,7 @@
 
             require_once 'app/config/configDb.php';
 
-            $this->conexion = new mysqli(SERVIDOR2,USUARIO2,PASSWORD2,BBDD2);
+            $this->conexion = new mysqli(SERVIDOR,USUARIO,PASSWORD,BBDD);
             $this->conexion->set_charset("utf8");
             
         }
